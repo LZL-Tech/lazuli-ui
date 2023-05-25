a44cdb2c95cf7b44dd9bada8385d3d0804629080
Elvis Liberato <liberatov13@gmail.com>
2023-05-24 21:43:37 -0300

    Inclusão de environment

--- src/app/venda/venda.service.ts
+++ src/app/venda/venda.service.ts
@@ -1,30 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Venda } from '../core/model/venda';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class VendaService {
 
 
-constructor(
-  private http: HttpClient
-  ) { }
+	constructor(private http: HttpClient) { }
 
-private readonly url = 'http://localhost:5000/venda'
+	private readonly api = environment.api;
+	private readonly url = `${this.api.protocol}://${this.api.host}:${this.api.port}/venda`
 
-findAll(): Observable<Venda[]> {
-  return this.http.get<Venda[]>(this.url)
-}
+	findAll(): Observable<Venda[]> {
+		return this.http.get<Venda[]>(this.url)
+	}
 
-findById(idVenda: number): Observable<Venda> {
-  return this.http.get<Venda>(`${this.url}/${idVenda}`)
-}
+	findById(idVenda: number): Observable<Venda> {
+		return this.http.get<Venda>(`${this.url}/${idVenda}`)
+	}
 
-save(venda: Venda) {
-  return this.http.post(this.url, venda.toJson())
-}
+	save(venda: Venda) {
+		return this.http.post(this.url, venda.toJson())
+	}
 }
 
